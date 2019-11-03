f8b1d7fd6c9e2b40b0b4ace6d18d82ee1096adb2
Gabriel Lucas <g.lucas.santos@gmail.com>
2019-11-03 11:33:06 -0300

    Create layout chat message

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -49,8 +50,119 @@ class _ChatScreenState extends State<ChatScreen> {
               Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
         ),
         body: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ChatMessage(),
+                  ChatMessage(),
+                  ChatMessage(),
+                ],
+              ),
+            ),
+            Divider(
+              height: 1.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: TextComposer(),
+            )
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  bool _isTyping = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[200])))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: TextField(
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma Mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isTyping = text.length > 0;
+                  });
+                },
+              ),
+            ),
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoButton(
+                      child: Text("Enviar"),
+                      onPressed: _isTyping ? () {} : null,
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: _isTyping ? () {} : null,
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(
+              right: 25.0,
+              left: 8.0,
+            ),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://image.freepik.com/fotos-gratis/mandando-um-copo_1258-340.jpg"),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text("Gabriel", style: Theme.of(context).textTheme.subhead),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text("Tudo bem"),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
